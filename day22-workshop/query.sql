9d61407c7710a41fd19a36eee88ae637e56e30e9
Chuk <chukmunnlee@gmail.com>
2019-11-12 16:58:55 +0800

    modified query.sql

--- day22-workshop/query.sql
+++ day22-workshop/query.sql
@@ -8,4 +8,22 @@ create view orders_summary as
     on c.id = o.customer_id
     join order_details od 
     on o.id = od.order_id
-    group by c.company
+    group by c.company;
+
+select s.company, 
+    count(s.order_id) as total_orders,
+    sum(s.quantity * (s.unit_price - s.discount)),
+    avg(s.quantity * (s.unit_price - s.discount))
+    from (
+        select c.company as company, 
+            o.id as order_id, 
+            od.quantity as quantity, 
+            od.unit_price as unit_price, 
+            od.discount as discount
+            from customers c
+            join orders o
+            on c.id = o.customer_id
+            join order_details od
+            on o.id = od.order_id
+    ) as s
+    group by s.company;
